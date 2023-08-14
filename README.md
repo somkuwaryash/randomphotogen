02032f546fde19a036554003c838888c6093f8fe
yash somkuwar <somkuwar.techfest@gmail.com>
2023-08-14 17:53:31 +0530

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# randomphotogen
+# RandomPhoto App
+
+An iOS application built in Swift and UIKit to fetch and display random photos from Unsplash.
+
+## Features
+
+- **Random Image Display**: Upon loading, the app fetches a random image of size 300x300 from Unsplash and displays it centered in the app.
+  
+- **Randomize Button**: Contains a button titled "Random Photo" that fetches another random image from Unsplash upon being clicked. 
+
+- **Dynamic Background**: Each time the button is pressed to fetch a new photo, the app's background color also changes randomly from a predefined list of colors.
+
+- **Auto Layout**: Leverages `viewDidLayoutSubviews()` to handle dynamic placement and sizing of UI components, making it adaptable for various device sizes and orientations.
+
+## Dependencies
+
+- **UIKit**: Utilized for the user interface components and layout.
+
+## To Do
+
+- Implement asynchronous methods for image fetching to optimize UI responsiveness.
+  
+- Enhance error handling for image fetching scenarios.
+
+- Incorporate more dynamic behavior, potentially animations, or other additional functionalities.
+
+> **Note**: Always respect API usage guidelines when making requests to platforms like Unsplash. Consider integrating their official SDK or handling rate limits if planning to scale or distribute the application.
--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -8,10 +8,69 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.contentMode = .scaleToFill
+        imageView.backgroundColor = .white
+        return imageView
+    }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Random Photo", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
+    let colors: [UIColor] = [
+        .systemRed,
+        .systemBlue,
+        .systemCyan,
+        .systemGray,
+        .systemPink,
+        .systemTeal,
+        .systemBrown
+    ]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x: 30, y: 250, width: 300, height: 300)
+        imageView.center = view.center
+        view.addSubview(button)
+        getRandomPhoto()
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    
+    @objc func didTapButton(){
+        getRandomPhoto()
+        view.backgroundColor = colors.randomElement()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+                button.frame = CGRect(
+            x: 10,
+            y: view.frame.size.height-250,
+            width: view.frame.size.height-500-view.safeAreaInsets.bottom,
+            height: 55
+        )
+
+    }
+    
+    func getRandomPhoto() {
+        let urlString =
+            "https://source.unsplash.com/random/300*300"
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
     }
 
 
